9d8393f49995f633cfce7866cba858f78e2a8104
Derek <derekviet01@tamu.edu>
2022-10-27 16:51:13 -0500

    FinalizeOrder and Backend Debugging

--- Backend/routes.js
+++ Backend/routes.js
@@ -39,32 +39,32 @@ function getUpdateQueryString (table_name, row_id, columns) {
 router.get('/Inventory', function(req, res, next) {
   client.query('SELECT * FROM Inventory', (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
 router.post('/Inventory', function(req, res, next) {
   const { Name, ItemType, Calories, Quantity, WholesaleCost, Minimum } = req.body;
 
-  pool.query('INSERT INTO Inventory (Name, ItemType, Calories, Quantity, WholesaleCost, Minimum) VALUES ($1, $2, $3, $4, $5, $6)', [Name, ItemType, Calories, Quantity, WholesaleCost, Minimum], (error, results) => {
+  client.query('INSERT INTO Inventory (name, itemtype, calories, quantity, wholesalecost, minimum) VALUES ($1, $2, $3, $4, $5, $6)', [Name, ItemType, Calories, Quantity, WholesaleCost, Minimum], (error, results) => {
     if (error) { throw error; }
-    response.status(201).send(`Row added with ID: ${results.rows[0].id}`)
+    res.sendStatus(201);
   })
 });
 
 router.put('/Inventory', function(req, res, next) {
   const { ID, Name, ItemType, Calories, Quantity, WholesaleCost, Minimum } = req.body;
   const { query_string, new_values } = getUpdateQueryString('Inventory', ID , {"Name": Name, "ItemType": ItemType, "Calories": Calories, "Quantity": Quantity, "WholesaleCost": WholesaleCost, "Minimum": Minimum});
-  pool.query(query_string, new_values, (error, results) => {
+  client.query(query_string, new_values, (error, results) => {
     if (error) { throw error; }
-    response.status(200).send('Row Updated')
+    res.status(200).send('Row Updated');
   })
 });
 
 router.get('/Inventory/:id', function(req, res, next) {
   client.query('SELECT * FROM Inventory WHERE id=$1', [req.params.id], (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
@@ -81,32 +81,32 @@ router.delete('/Inventory/:id', function(req, res, next) {
 router.get('/Sizes', function(req, res, next) {
   client.query('SELECT * FROM Sizes', (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
 router.post('/Sizes', function(req, res, next) {
   const { Name, NumSides, NumEntrees, Price } = req.body;
 
-  pool.query('INSERT INTO Sizes (Name, NumSides, NumEntrees, Price) VALUES ($1, $2, $3, $4)', [Name, NumSides, NumEntrees, Price], (error, results) => {
+  client.query('INSERT INTO Sizes (Name, NumSides, NumEntrees, Price) VALUES ($1, $2, $3, $4)', [Name, NumSides, NumEntrees, Price], (error, results) => {
     if (error) { throw error; }
-    response.status(201).send(`Row added with ID: ${results.rows[0].id}`)
+    res.sendStatus(201);
   })
 });
 
 router.put('/Sizes', function(req, res, next) {
   const { ID, Name, NumSides, NumEntrees, Price } = req.body;
   const { query_string, new_values } = getUpdateQueryString('Sizes', ID , {"Name": Name, "NumSides": NumSides, "NumEntrees": NumEntrees, "Price": Price});
-  pool.query(query_string, new_values, (error, results) => {
+  client.query(query_string, new_values, (error, results) => {
     if (error) { throw error; }
-    response.status(200).send('Row Updated')
+    res.status(200).send('Row Updated');
   })
 });
 
 router.get('/Sizes/:id', function(req, res, next) {
   client.query('SELECT * FROM Sizes WHERE id=$1', [req.params.id], (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
@@ -124,32 +124,32 @@ router.delete('/Sizes/:id', function(req, res, next) {
 router.get('/OrderHistory', function(req, res, next) {
   client.query('SELECT * FROM OrderHistory', (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
 router.post('/OrderHistory', function(req, res, next) {
   const { Date, ServerName, CustomerName, TotalPrice, OrderDetails } = req.body;
 
-  pool.query('INSERT INTO OrderHistory (Date, ServerName, CustomerName, TotalPrice, OrderDetails) VALUES ($1, $2, $3, $4, $5)', [Date, ServerName, CustomerName, TotalPrice, OrderDetails], (error, results) => {
+  client.query('INSERT INTO OrderHistory (Date, ServerName, CustomerName, TotalPrice, OrderDetails) VALUES ($1, $2, $3, $4, $5)', [Date, ServerName, CustomerName, TotalPrice, OrderDetails], (error, results) => {
     if (error) { throw error; }
-    response.status(201).send(`Row added with ID: ${results.rows[0].id}`)
+    res.sendStatus(201);
   })
 });
 
 router.put('/OrderHistory', function(req, res, next) {
   const { ID, Date, ServerName, CustomerName, TotalPrice, OrderDetails } = req.body;
   const { query_string, new_values } = getUpdateQueryString('OrderHistory', ID , {"Date": Date, "ServerName": ServerName, "CustomerName": CustomerName, "TotalPrice": TotalPrice, "OrderDetails": OrderDetails});
-  pool.query(query_string, new_values, (error, results) => {
+  client.query(query_string, new_values, (error, results) => {
     if (error) { throw error; }
-    response.status(200).send('Row Updated')
+    res.status(200).send('Row Updated');
   })
 });
 
 router.get('/OrderHistory/:id', function(req, res, next) {
   client.query('SELECT * FROM OrderHistory WHERE id=$1', [req.params.id], (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
@@ -167,32 +167,32 @@ router.delete('/OrderHistory/:id', function(req, res, next) {
 router.get('/Employees', function(req, res, next) {
   client.query('SELECT * FROM Employees', (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
 router.post('/Employees', function(req, res, next) {
   const { Name, Role, Username, Password } = req.body;
 
-  pool.query('INSERT INTO Employees (Name, Role, Username, Password) VALUES ($1, $2, $3, $4)', [Name, Role, Username, Password], (error, results) => {
+  client.query('INSERT INTO Employees (Name, Role, Username, Password) VALUES ($1, $2, $3, $4)', [Name, Role, Username, Password], (error, results) => {
     if (error) { throw error; }
-    response.status(201).send(`Row added with ID: ${results.rows[0].id}`)
+    res.sendStatus(201);
   })
 });
 
 router.put('/Employees', function(req, res, next) {
   const { ID, Name, Role, Username, Password } = req.body;
   const { query_string, new_values } = getUpdateQueryString('Employees', ID , {"Name": Name, "Role": Role, "Username": Username, "Password": Password});
-  pool.query(query_string, new_values, (error, results) => {
+  client.query(query_string, new_values, (error, results) => {
     if (error) { throw error; }
-    response.status(200).send('Row Updated')
+    res.status(200).send('Row Updated');
   })
 });
 
 router.get('/Employees/:id', function(req, res, next) {
   client.query('SELECT * FROM Employees WHERE id=$1', [req.params.id], (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
@@ -210,7 +210,7 @@ router.delete('/Employees/:id', function(req, res, next) {
 router.get('/InventoryHistory', function(req, res, next) {
   client.query('SELECT * FROM InventoryHistory', (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 
 });
@@ -218,25 +218,25 @@ router.get('/InventoryHistory', function(req, res, next) {
 router.post('/InventoryHistory', function(req, res, next) {
   const { Date, CurrentInventory } = req.body;
 
-  pool.query('INSERT INTO InventoryHistory (Date, CurrentInventory) VALUES ($1, $2)', [Date, CurrentInventory], (error, results) => {
+  client.query('INSERT INTO InventoryHistory (Date, CurrentInventory) VALUES ($1, $2)', [Date, CurrentInventory], (error, results) => {
     if (error) { throw error; }
-    response.status(201).send(`Row added with ID: ${results.rows[0].id}`)
+    res.sendStatus(201);
   })
 });
 
 router.put('/InventoryHistory', function(req, res, next) {
   const { ID, Date, CurrentInventory } = req.body;
   const { query_string, new_values } = getUpdateQueryString('InventoryHistory', ID , {"Date": Date, "CurrentInventory": CurrentInventory});
-  pool.query(query_string, new_values, (error, results) => {
+  client.query(query_string, new_values, (error, results) => {
     if (error) { throw error; }
-    response.status(200).send('Row Updated')
+    res.status(200).send('Row Updated');
   })
 });
 
 router.get('/InventoryHistory/:id', function(req, res, next) {
   client.query('SELECT * FROM InventoryHistory WHERE id=$1', [req.params.id], (error, results) => {
     if (error) { throw error; }
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
@@ -252,32 +252,77 @@ router.delete('/InventoryHistory/:id', function(req, res, next) {
 
 /* Special Routes */
 
-/*Input: JSON array. Example below
-[
-  {
-    Size: "bowl",
-    Items: ["Orange Chicken", "Chow Mein"]
-  }, 
-  {
-    Size: "plate",
-    Items: ["Orange Chicken", "Orange Chicken", "Fried Rice"]
-  } 
-]
+/*Input: JSON object with keys for each non-ID/non-Date column in OrderHistory. Example below
+{
+  "ServerName": "Steve",
+  "CustomerName": "James",
+  "TotalPrice": 1.9,
+  "OrderDetails": [
+    {
+      "Size": "bowl",
+      "Items": ["Orange Chicken", "Chow Mein"]
+    }, 
+    {
+      "Size": "plate",
+      "Items": ["Orange Chicken", "Orange Chicken", "Fried Rice"]
+    } 
+  ]
+}
+
 */
 router.post('/FinalizeOrder', function(req, res, next) {
-  res.send('respond with a resource');
+  //OrderHistory Update
+  const { ServerName, CustomerName, TotalPrice, OrderDetails } = req.body;
+  client.query('INSERT INTO OrderHistory (Date, ServerName, CustomerName, TotalPrice, OrderDetails) VALUES (CURRENT_DATE, $1, $2, $3, $4)', [ServerName, CustomerName, TotalPrice, OrderDetails], (error, results) => {
+    if (error) { throw error; }
+  });
+
+  //Inventory Update
+  ordered_items = [];
+  for (let i = 0; i < OrderDetails.length; i++) {
+    ordered_items = ordered_items.concat(OrderDetails[i].Items);
+  }
+
+  ordered_item_count = {};
+  for (let index in ordered_items) {
+    ordered_item_count[ordered_items[index]] = ordered_item_count[ordered_items[index]] + 1 || 1;
+  }
+
+  for (const [item_name, count] of Object.entries(ordered_item_count)) {
+    //Get current quantity of each item
+    client.query('SELECT * FROM Inventory WHERE Name=$1', [item_name], (select_error, select_results) => {
+      if (select_error) { throw select_error; }
+      //console.log(select_results);
+      
+      //Update each item with new quantity
+      client.query("UPDATE Inventory SET Quantity=$1 WHERE Name=$2", [parseInt(select_results.rows[0].quantity) - count, item_name], (update_error, update_results) => {
+        if (update_error) { throw update_error; }
+      })
+    });
+  }
+  
+  res.status(201).send('Order finalized');
 });
 
-router.post('/Login', function(req, res, next) {
-  client.query('SELECT * FROM InventoryHistory WHERE username=$1 AND password=$2', [req.query.username, req.query.password], (error, results) => {
+router.post('/EmployeeLogin', function(req, res, next) {
+  client.query('SELECT * FROM Employees WHERE username=$1 AND password=$2', [req.query.username, req.query.password], (error, results) => {
     if (error) { throw error; }
     //TODO: If result.rows.length is 0, send failure.
-    response.sendStatus(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 });
 
-router.post('/Register', function(req, res, next) {
+router.post('/CustomerLogin', function(req, res, next) {
+  client.query('SELECT * FROM CustomerAccounts WHERE username=$1 AND password=$2', [req.query.username, req.query.password], (error, results) => {
+    if (error) { throw error; }
+    //TODO: If result.rows.length is 0, send failure.
+    res.status(200).json(results.rows);
+  });
+});
 
+router.post('/RegisterCustomerAccount', function(req, res, next) {
+  //Check if username is unique
+  //Create new account
 });
 
 
