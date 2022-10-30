7077595188eabfff401ca517d8e500fdbfb76c88
William Kang <wkang2020@gmail.com>
2022-10-30 18:10:14 -0500

    connected index html page to js

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendFile("./Frontend/index.html", {root: "../"}); // sending request from app.js I guess
 });
 
 module.exports = router;
