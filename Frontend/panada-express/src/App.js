7c41c1ee2be35edda0eeb6ab9da5086f540fc80e
boxkr <keeg9087@gmail.com>
2022-10-31 18:20:23 -0500

    change port, playing around

--- Frontend/panada-express/src/App.js
+++ Frontend/panada-express/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+let x = "this is a variable";
 function App() {
+
+  //createVariables();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {x}
         </p>
         <a
           className="App-link"
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          dont Learn React
         </a>
       </header>
     </div>
