313898e50157b89406801c872e82c582f69bc5b6
Derek <derekviet01@tamu.edu>
2022-10-31 16:10:54 -0500

    Reversed Last Changes
    - Removed routes folder and js files
    - Removed empty EnvironmentVariables.txt file

--- Backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile("./Frontend/index.html", {root: "../"}); // sending request from app.js I guess
-});
-
-module.exports = router;
