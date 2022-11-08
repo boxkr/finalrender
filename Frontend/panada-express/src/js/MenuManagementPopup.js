cba0d97327c9a079379c73c454ba33fe71ec7230
boxkr <keeg9087@gmail.com>
2022-11-07 20:57:21 -0600

    menu management

--- Frontend/panada-express/src/js/MenuManagementPopup.js
+++ Frontend/panada-express/src/js/MenuManagementPopup.js
@@ -1,35 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import '../css/managerstyles.css';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {Button} from 'react-bootstrap';
 
 
-const AdjustPrice=()=>{
-
-
-    return(
-        <div>
-            <h1>Adjust size prices placeholder</h1>
-        </div>
-    )
-}
-
-const AddNewItem=()=>{
-
-    return(
-        <div>
-            <h1>Add a new item placeholder</h1>
-        </div>
-    )
-}
-const CurrentMenu=()=>{
-
-    return(
-        <div>
-            <h1>Placeholder for current menu map</h1>
-        </div>
-    )
-}
 export default function MenuManagementPopup(props) {
 
     const handleClose=()=>{
@@ -37,14 +11,71 @@ export default function MenuManagementPopup(props) {
     }
     console.log(props);
 
-    
+    const [sizePrice,setSizePrice] = useState("");
+    const [sizeName, setSizeName] = useState("");
+    const [newItemName, setNewItemName] = useState("")
+    const [newItemType, setNewItemType] = useState("")
+    const [newItemCalories, setNewItemCalories] = useState("")
+    const [newItemQuantity, setNewItemQuantity] = useState("")
+    const [newItemWC,setNewItemWC] = useState("")  //wholesale cost
+    const [currentSizes,setCurrentSizes] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/Sizes")
+            .then((response) => response.json())
+            .then((data) => setCurrentSizes(data)); 
+    }, []);
+
+    const handleSizeEditSubmit=(e)=>{
+        e.preventDefault()
+        console.log(sizeName,sizePrice)
+    }
+    const handleNewItem=(e)=>{
+        e.preventDefault()
+        console.log(newItemName,newItemType,newItemCalories,newItemQuantity,newItemWC)
+    }
     return (
         <div className="management-container">
             <Button onClick={handleClose}>Close</Button>
             <h1 className='management-header'>Menu Management</h1>
-            <AdjustPrice/>
-            <AddNewItem/>
-            <CurrentMenu/>
+            <div>
+                <h2 className='restock-header'>Edit Size Price:</h2>
+                <form>
+                    <label className='restock-text'>Size Name: </label>
+                    <input type='text' value={sizeName} onChange={(e)=>{setSizeName(e.target.value)}}/>
+                    <label className='restock-text'>New Size Price:</label>
+                    <input type='text' value={sizePrice} onChange={(e)=>{setSizePrice(e.target.value)}}/>
+                    <input value="Submit Edit" type='submit' onClick={handleSizeEditSubmit}/>
+                </form>
+            </div>
+            <div>
+                <h2 className='restock-header'>Create a new item</h2>
+                <form>
+                    <label className='restock-text'>New Item Name: </label>
+                    <input type='text' value={newItemName} onChange={(e)=>{setNewItemName(e.target.value)}}/>
+                    <label className='restock-text'>New Item Type:</label>
+                    <input type='text' value={newItemType} onChange={(e)=>{setNewItemType(e.target.value)}}/>
+                    <label className='restock-text'>New Item Quantity:</label>
+                    <input type='text' value={newItemQuantity} onChange={(e)=>{setNewItemQuantity(e.target.value)}}/>
+                    <br/>
+                    <label className='restock-text'>New Item Calories:</label>
+                    <input type='text' value={newItemCalories} onChange={(e)=>{setNewItemCalories(e.target.value)}}/>
+                    <label className='restock-text'>New Item Wholesale Cost:</label>
+                    <input type='text' value={newItemWC} onChange={(e)=>{setNewItemWC(e.target.value)}}/>
+                    <br/>
+                    <input value="Create new item" type='submit' onClick={handleNewItem}/>
+                </form>
+            </div>
+            {console.log(currentSizes)}
+            <div className='sizes-view'>
+                {currentSizes.map((size) => (
+                    <div id={size.name} key={size.id}>
+                        <p className='inventory-item'>{size.name}</p>
+                        <p className='inventory-item'>{size.price}</p>
+                    </div>
+                ))}
+            </div>
+            
         </div>
     )
 }
