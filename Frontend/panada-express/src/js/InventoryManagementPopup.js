383ab643eb36c92c04a0b8fd0d1321b0448242b2
boxkr <keeg9087@gmail.com>
2022-11-07 18:26:35 -0600

    scrum3, managment inventory populate

--- Frontend/panada-express/src/js/InventoryManagementPopup.js
+++ Frontend/panada-express/src/js/InventoryManagementPopup.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../css/managerstyles.css';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {Button} from 'react-bootstrap';
@@ -13,27 +13,38 @@ const RestockInventory=()=>{
     )
 }
 
-const ViewInventory=()=>{
-    return(
-        <div>
-            <h1>Placeholder for inventory view</h1>
-        </div>
-    )
-}
+
 
 export default function InventoryManagementPopup(props) {
 
     const handleClose=()=>{
         props.popCounterFunction(props.popupCounter+1);
     }
-    console.log(props);
+
+    const [fullInventory, setFullInventory] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/api/Inventory")
+            .then((response) => response.json())
+            .then((data) => setFullInventory(data)); 
+    }, []);
+
+    
+    console.log(fullInventory);
     
     return (
         <div className="management-container">
             <Button onClick={handleClose}>Close</Button>
             <h1 className="management-header">Inventory Management</h1>
             <RestockInventory/>
-            <ViewInventory/>
+            <div className="inventory-view">
+                {fullInventory.map((item) => (
+                    <div id={item.name} key={item.id}>
+                        <p className='inventory-item'>{item.name}</p>
+                        <p className='inventory-item'>{item.quantity}</p>
+                    </div>
+                ))}
+            </div>
+            
         </div>
     )
 }
--- Frontend/panada-express/src/js/Manager.js
+++ Frontend/panada-express/src/js/Manager.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import '../css/managerstyles.css';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {Button} from 'react-bootstrap';
@@ -11,17 +11,24 @@ export default function Manager() {
     
     const [menuPopupCounter, setMenuPopupCounter] = useState(1);
     const [inventoryPopupCounter, setInventoryPopupCounter] = useState(1);
+
+    
+
+    
     const handleMenuClick = ()=>{
-        
         //default is 1 (odd) so closed at first. if we see it's clicked and odd, then we want to open the menu, else close it
         setMenuPopupCounter(menuPopupCounter+1);
     }
+
     const handleInventoryClick=()=>{
         setInventoryPopupCounter(inventoryPopupCounter+1);
     }
     
+    
+
     return (
 
+        
         <div className="container">
             <h1 className="welcome"> Welcome Manager! </h1>
             <br></br>
